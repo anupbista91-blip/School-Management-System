b17f0600e1a1d3b41c901c009e193fa14186e11d
anupbista91-blip <anupbista91@gmail.com>
2026-03-24 09:25:15 +0545

    Create Constants.java

---
+++ src/main/java/School-Management-System/Util/Constants.java
@@ -0,0 +1,13 @@
+/**
+ * File: Constants.java
+ * Purpose: Application-wide constants.
+ */
+package School_Management_System.Util;
+
+/**
+ * Constants used across the app.
+ */
+public class Constants {
+    public static final String APP_NAME = "School Management System";
+    public static final int DEFAULT_PORT = 5607;
+}
